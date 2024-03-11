db4d2c3e2985d9562a92ad97413c1df16e706592
Charly021595 <charlydelacruzmartinez@gmail.com>
2024-03-10 22:47:38 -0600

    Actualizacion_2024_03_10

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { FieldsetModule } from 'primeng/fieldset';
 import { MenubarModule } from 'primeng/menubar';
-// import { MenuModule } from 'primeng/menu';
+import { PanelModule } from 'primeng/panel';
+
 
 @NgModule({
   exports: [
-    // MenuModule
-    MenubarModule
+    ButtonModule,
+    CardModule,
+    FieldsetModule,
+    MenubarModule,
+    PanelModule,
   ]
 })
 export class PrimeNgModule { }
--- src/app/products/pages/basics-page/basics-page.component.ts
+++ src/app/products/pages/basics-page/basics-page.component.ts
@@ -2,11 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-basics-page',
-  standalone: true,
-  imports: [],
   templateUrl: './basics-page.component.html',
   styleUrl: './basics-page.component.css'
 })
 export class BasicsPageComponent {
+
+  public nameLower: string = 'fernando';
+  public nameUpper: string = 'FERNANDO';
+  public fullname: string = 'fErNAndo hErReRa';
+
+  public customDate: Date = new Date();
 
 }
